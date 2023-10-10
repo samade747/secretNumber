f48695b064b889854b3ad866e41c046dc59e9625
samade747 <samad.e747@gmail.com>
2023-10-11 00:09:01 +0500

    11

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let secretNumber = Math.floor(Math.random() * 10) +1;
 let userInput = parseInt(prompt(`Guess the secret number between 1 & 10`));
 
 if (userInput === secretNumber){
-    document.getElementById('result').innerHTML = `<p class="result">Congratulations! You guessed the secret Number: ${secretNumber}</p>`;  
+    document.getElementById('result').innerHTML = (`<p class="result">Congratulations! You guessed the secret Number: ${secretNumber}</p>`);  
 } else {
-    document.getElementById('result').innerHTML = `<p class="result">Sorry, the secret number was ${secretNumber}</p>`;
+    document.getElementById('result').innerHTML = (`<p class="result">Sorry, the secret number was ${secretNumber}</p>`);
 }
